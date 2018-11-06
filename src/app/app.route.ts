526593ae75e5a637bd6cb0067166b86b67e5220b
Jesús Fco Cortés <jfcr@live.com>
2018-11-06 10:51:14 -0600

    Creating new Event Component

--- src/app/app.route.ts
+++ src/app/app.route.ts
@@ -83,7 +83,7 @@ export const routes: Routes = [
                     { path: 'delete', component: DeleteUserComponent}
             ]},
         ]
-    },
+    },    
 
     {
         path: 'receipt',
--- src/app/event/create-event/create-event.component.ts
+++ src/app/event/create-event/create-event.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { EventService } from './../event.service';
+import { cardPop, backgroundOpacity } from './../../animations';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
+import { Event } from '../../classes/event';
 
 @Component({
   selector: 'app-create-event',
   templateUrl: './create-event.component.html',
-  styleUrls: ['./create-event.component.css']
+  styleUrls: ['./create-event.component.css'],
+  animations: [cardPop, backgroundOpacity],
 })
 export class CreateEventComponent implements OnInit {
 
-  constructor() { }
+  @HostListener('document:keyup', ['$event']) sss($event) {
+    
+    if($event.keyCode === 27) {
+      this.closePop();
+    }
+  }
+
+  public state = {
+    background: 'initial',
+    card: 'initial',
+  }
+
+  public event: Event = new Event();
+
+  constructor(private _http: EventService, private router: Router) { }
 
   ngOnInit() {
+
+    setTimeout(() => {
+      this.state.background = 'final';
+      this.state.card = 'final';
+    }, 10);
+
+  }
+
+  closePop() {
+
+    setTimeout(() => {
+      this.router.navigate(['../']);
+    }, 450);
+    this.state.background = 'initial';
+    this.state.card = 'initial';
+    
   }
 
 }
