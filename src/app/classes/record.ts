c9b51fcffa260f8c30dd7bec7a84c5e83e63fa3e
Jesús Fco Cortés <jfcr@live.com>
2020-02-23 13:50:12 -0600

    Changes On Record Show

--- src/app/classes/record.ts
+++ src/app/classes/record.ts
@@ -1,25 +1,38 @@
 import { User } from "./user";
+import { ObjectJSONParser } from "../utils/classes/ObjectJSON";
 
 export class Record { 
-    public id: number;
-    public user_id: number;
-    public user: User = new User();
-    public checkIn: String;
-    public checkOut: String;
-    public date: String;
+    id: number;
+    user_id: number;
+    user: User = new User();
+    checkIn: String = '';
+    checkOut: String = '';    
+    date: String = '';
+    time_worked: String= ''
+    time_extra: String  = '';
+    type: number = 0
+    observation: String = ''
+
+    
     
     constructor() {
 
     }
 
     setValues(data) {
-        this.id = parseInt(data.id);
-        this.user_id = parseInt(data.user_id);
+        this.id = 0
+        this.user_id = 0
+        ObjectJSONParser.set(data, this)
         if(data.user != undefined) {
-            this.user.setValues(data.user);
+            this.user.setData(data.user);
         }
-        this.date = data.date;
-        this.checkIn = data.checkIn;
-        this.checkOut = data.checkOut;
+        
+    }
+
+    typeView() {
+        if(this.type == 0) return 'Normal'
+        if(this.type == 1) return 'Entrada Previa'
+        if(this.type == 2) return 'Sin horario relacionado'
+        return ''
     }
 }
